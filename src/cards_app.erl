cd02b17e410966eaa215be8028f150ac67af7ec2
Hari <harisankar@qburst.com>
2014-12-24 13:49:42 +0530

    patch

--- src/cards_app.erl
+++ src/cards_app.erl
@@ -11,9 +11,9 @@
 
 start(_StartType, _StartArgs) ->
   Dispatch = cowboy_router:compile([
-    {'_', [{"/[:controller/:action]", cards_dealer, []}]}
+    {'_', [{"/[:controller/:action/:extras]", cards_dealer, []}]}
   ]),
-  cowboy:start_http(my_http_listener, 100, [{port, application:get_env(application:get_application(), cowboy_port, 8080)}],
+  cowboy:start_http(my_http_listener, 100, [{port, application:get_env(cards, cowboy_port, 8080)}],
     [{env, [{dispatch, Dispatch}]}]
   ),
   cards_sup:start_link().
