08556474d9edacb2a1aabdd2f464374c2194fee6
Sandeep Unni <psandeepunni@gmail.com>
2014-12-24 20:20:32 +0530

    added support for access policies

--- src/cards_dealer.erl
+++ src/cards_dealer.erl
@@ -11,6 +11,12 @@
 init(_, Req, _Opts) ->
   {ok, Req, #state{}}.
 
+%%%============================================================================
+%%% Function : get_module
+%%% Params : Controller/binary
+%%% Return : Controller/atom
+%%%============================================================================
+
 get_module(Controller) ->
   Part = <<"_controller">>,
   ModuleName = <<Controller/binary,Part/binary>>,
@@ -21,6 +27,12 @@ get_module(Controller) ->
       binary_to_existing_atom(ModuleName, utf8)
   end.
 
+%%%============================================================================
+%%% Function : get_function
+%%% Params : Action/binary
+%%% Return : Action/atom
+%%%============================================================================
+
 get_function(Action) ->
   case catch binary_to_existing_atom(Action, utf8) of
     {'EXIT', {badarg,_}} ->
@@ -29,22 +41,89 @@ get_function(Action) ->
       binary_to_existing_atom(Action,utf8)
   end.
 
-response(<<"OPTIONS">>, Req, State) ->
+%%%==========================================================================================================
+%%% Function : response
+%%% Params : Status/atom, {Method/binary, Controller/binary, Action/binary}, Req/Term, Opts/Term, State/Term
+%%% Return : Action/atom
+%%%==========================================================================================================
+
+response(error, {Method, Controller, Action}, Req, Opts, State) ->
+  {ok, Req1} = cowboy_req:reply(200,[],<<"you are not authorized to access the api">>, Req),
+  {ok, Req1, State};
+response(ok, {<<"OPTIONS">>, Controller, Action}, Req, Opts, State) ->
   {Status, Req1} = cowboy_req:reply(200,[{<<"Access-Control-Allow-Headers">>,<<"origin, x-csrftoken, content-type, accept">>}],<<"">>,Req),
   {Status, Req1, State};
-response(Method, Req, State) ->
-  {Controller,_} = cowboy_req:binding(controller,Req),
-  {Action,_} = cowboy_req:binding(action,Req),
+response(ok, {Method, Controller, Action}, Req, Opts, State) ->
   Module = get_module(Controller),
   Func = get_function(Action),
-  {Status, Req1} = apply(Module, Func, [Method,Req]),
+  {Status, Req1} = apply(Module, Func, [Method,Req, Opts]),
   {Status, Req1, State}.
+
+%%%==========================================================================================================
+%%% Evaluate & Execute Chained Access Policies
+%%% Function : evaluate_access_policy
+%%% Params : Status/atom, PolicyList/list, Req/Term, Opts/Term,
+%%% Return : Status/atom, Req/Term, Opts/term
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Function : execute_access_policy
+%%% Params : Status/atom, Req/Term
+%%% Return : Status/atom, Req/Term, Opts/term
+%%%==========================================================================================================
+
+evaluate_access_policy(Status,[],Req,Opts) ->
+  {Status, Req, Opts};
+evaluate_access_policy(error,_,Req, Opts) ->
+  {error, Req, Opts};
+evaluate_access_policy(ok, [Policy|T], Req, Opts) ->
+  {Status, Req1, Opts1} = apply(Policy, execute, [Req]),
+  evaluate_access_policy(Status, T, Req1, [Opts1 | Opts]).
+
+execute_access_policy([true], Req) ->
+  {ok, Req, []};
+execute_access_policy([false], Req) ->
+  {error, Req, []};
+execute_access_policy(PolicyModuleList, Req) ->
+  evaluate_access_policy(ok, PolicyModuleList, Req, []).
+
+%%%==========================================================================================================
+%%% Function : get_action_policy
+%%% Params : Status/atom, Action/binary, PolicyMap/Term, Default/atom
+%%% Return : ModuleName/atom
+%%%==========================================================================================================
+
+get_action_policy(error, Action, PolicyMap, Default) ->
+  [Default];
+get_action_policy(ok, Action, PolicyMap, Default) ->
+  {Status,ActionPolicyModuleList} = maps:find(Action,PolicyMap),
+  case Status of
+    ok ->
+      ActionPolicyModuleList;
+    error ->
+      [Default]
+  end.
+
+%%%==========================================================================================================
+%%% Function : get_policies
+%%% Params : Req/term, Controller/binary, Action/binary
+%%% Return : ModuleName/atom
+%%%==========================================================================================================
+
+get_policies(Req, Controller, Action) ->
+  Policies = policies:get(),
+  {ok, DefaultPolicy} = maps:find(<<"default">>,Policies),
+  {ControllerPolicyFoundStatus, ControllerPolicyMap} = maps:find(Controller,Policies),
+  PolicyModuleNameList = get_action_policy(ControllerPolicyFoundStatus, Action, ControllerPolicyMap, DefaultPolicy),
+  PolicyModuleNameList.
 
 handle(Req, State=#state{}) ->
   {Method, Req1} = cowboy_req:method(Req),
+  {Controller,_} = cowboy_req:binding(controller,Req1),
+  {Action,_} = cowboy_req:binding(action,Req1),
   Req2 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, OPTIONS">>, Req1),
   Req3 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req2),
-  response(Method,Req3, State).
+  PolicyModuleNameList = get_policies(Req3, Controller, Action),
+  {AccessStatus, Req4, Opts} = execute_access_policy(PolicyModuleNameList, Req3),
+  response(AccessStatus, {Method, Controller, Action}, Req4, Opts, State).
 
 
 terminate(_Reason, _Req, _State) ->
