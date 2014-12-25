35accb22b993538c17b1f1b10ae7aca87872478c
Sandeep Unni <psandeepunni@gmail.com>
2014-12-25 23:17:46 +0530

    merged new changes, added cards root handler, cards access policy module

--- src/cards_app.erl
+++ src/cards_app.erl
@@ -11,7 +11,10 @@
 
 start(_StartType, _StartArgs) ->
   Dispatch = cowboy_router:compile([
-    {'_', [{"/[:controller/:action/:extras]", cards_dealer, []}]}
+    {'_', [
+    {"/:controller/:action/[:extras]", cards_dealer, []},
+    {"/", cards_root_handler, []}
+    ]}
   ]),
   cowboy:start_http(my_http_listener, 100, [{port, application:get_env(cards, cowboy_port, 8080)}],
     [{env, [{dispatch, Dispatch}]}]
