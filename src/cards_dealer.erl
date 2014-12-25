cc358f20d81bcba2cc7f4297feccb794ddaa0722
Sandeep Unni <psandeepunni@gmail.com>
2014-12-25 18:43:38 +0530

    added default cascading of policies, if none are defined

--- src/cards_dealer.erl
+++ src/cards_dealer.erl
@@ -47,16 +47,16 @@ get_function(Action) ->
 %%% Return : Action/atom
 %%%==========================================================================================================
 
-response(error, {Method, Controller, Action}, Req, Opts, State) ->
+response(error, _, Req, _, State) ->
   {ok, Req1} = cowboy_req:reply(200,[],<<"you are not authorized to access the api">>, Req),
   {ok, Req1, State};
-response(ok, {<<"OPTIONS">>, Controller, Action}, Req, Opts, State) ->
+response(ok, {<<"OPTIONS">>, _, _}, Req, _, State) ->
   {Status, Req1} = cowboy_req:reply(200,[{<<"Access-Control-Allow-Headers">>,<<"origin, x-csrftoken, content-type, accept">>}],<<"">>,Req),
   {Status, Req1, State};
 response(ok, {Method, Controller, Action}, Req, Opts, State) ->
   Module = get_module(Controller),
   Func = get_function(Action),
-  {Status, Req1} = apply(Module, Func, [Method,Req, Opts]),
+  {Status, Req1} = apply(Module, Func, [Method, Req, Opts]),
   {Status, Req1, State}.
 
 %%%==========================================================================================================
@@ -91,33 +91,33 @@ execute_access_policy(PolicyModuleList, Req) ->
 %%% Return : ModuleName/atom
 %%%==========================================================================================================
 
-get_action_policy(error, Action, PolicyMap, Default) ->
-  Default;
-get_action_policy(ok, Action, PolicyMap, Default) ->
-  Result = maps:find(Action,PolicyMap),
+get_action_policy(error, _, _, GlobalDefault) ->
+  GlobalDefault;
+get_action_policy(ok, Action, PolicyMap, GlobalDefault) ->
+  Result = maps:find(Action, PolicyMap),
   case Result of
     error ->
-      Default;
+      maps:get(<<"default">>, PolicyMap, GlobalDefault);
     _ ->
-      {Status,ActionPolicyModuleList} = Result,
+      {_, ActionPolicyModuleList} = Result,
       ActionPolicyModuleList
-    end.
+  end.
 
 %%%==========================================================================================================
 %%% Function : get_policies
-%%% Params : Req/term, Controller/binary, Action/binary
+%%% Params : Controller/binary, Action/binary
 %%% Return : ModuleName/atom
 %%%==========================================================================================================
 
-get_policies(Req, Controller, Action) ->
+get_policies(Controller, Action) ->
   Policies = policies:get(),
-  {ok, DefaultPolicy} = maps:find(<<"default">>,Policies),
+  GlobalDefaultPolicy = maps:get(<<"default">>,Policies,[true]),
   Result = maps:find(Controller,Policies),
   {ControllerPolicyFoundStatus, ControllerPolicyMap} =  case Result of
                                                           error -> {error, #{}};
                                                           _ -> Result
                                                         end,
-  PolicyModuleNameList = get_action_policy(ControllerPolicyFoundStatus, Action, ControllerPolicyMap, DefaultPolicy),
+  PolicyModuleNameList = get_action_policy(ControllerPolicyFoundStatus, Action, ControllerPolicyMap, GlobalDefaultPolicy),
   PolicyModuleNameList.
 
 handle(Req, State=#state{}) ->
@@ -126,8 +126,7 @@ handle(Req, State=#state{}) ->
   {Action,_} = cowboy_req:binding(action,Req1),
   Req2 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, OPTIONS">>, Req1),
   Req3 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req2),
-  PolicyModuleNameList = get_policies(Req3, Controller, Action),
-  io:format('policy module list : ~p~n',[PolicyModuleNameList]),
+  PolicyModuleNameList = get_policies(Controller, Action),
   {AccessStatus, Req4, Opts} = execute_access_policy(PolicyModuleNameList, Req3),
   response(AccessStatus, {Method, Controller, Action}, Req4, Opts, State).
 
