c3bf5655616d75fe7a361d2664352f033e088f53
Jakub Czarniecki <jakubc@qburst.com>
2015-02-05 12:22:13 +0100

    Change CORS headers policy to request headers for compatibility.

--- src/cards_root_handler.erl
+++ src/cards_root_handler.erl
@@ -16,7 +16,8 @@ handle(Req, State=#state{}) ->
   {Method, Req1} = cowboy_req:method(Req),
   Req2 = cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"GET, POST, PUT, DELETE, OPTIONS">>, Req1),
   Req3 = cowboy_req:set_resp_header(<<"access-control-allow-origin">>, <<"*">>, Req2),
-  Req4 = cowboy_req:set_resp_header(<<"access-control-allow-headers">>, <<"*">>, Req3),
+  {RequestHeaders, Req4} = cowboy_req:header(<<"access-control-request-headers">>, Req3, <<"*">>),
+  Req5 = cowboy_req:set_resp_header(<<"access-control-allow-headers">>, RequestHeaders, Req4),
   PolicyModuleNameList = access_policy:get(<<"httproot">>, <<"index">>),
   {AccessStatus, Req5, Opts} = access_policy:evaluate(PolicyModuleNameList, Req4),
   cards_res:reply(AccessStatus, {Method, <<"httproot">>, <<"index">>}, Req5, Opts, State).
